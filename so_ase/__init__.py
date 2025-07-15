5a6d135cf751b72849cc975a190f6f9782046c94
Finn Heukamp <Finn.Heukamp@t-online.de>
2025-07-15 10:48:20 +0200

    added fesom_ocean_heat_transport_as_residual for import

--- so_ase/__init__.py
+++ so_ase/__init__.py
@@ -4,3 +4,4 @@ __version__ = "0.1.0"
 
 from .plotting_maps import create_map
 from .eval_sea_ice import fesom_ice_area, fesom_ice_volume
+from .eval_ocean import fesom_ocean_heat_transport_as_residual
