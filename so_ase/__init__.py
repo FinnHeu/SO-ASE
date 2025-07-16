583725e5f23f49714bd124b577ad21a9807a59c7
Finn Heukamp <Finn.Heukamp@t-online.de>
2025-07-16 15:56:42 +0200

    added function to __init__.py

--- so_ase/__init__.py
+++ so_ase/__init__.py
@@ -4,4 +4,4 @@ __version__ = "0.1.0"
 
 from .plotting_maps import create_map
 from .eval_sea_ice import fesom_ice_area, fesom_ice_volume
-from .eval_ocean import fesom_ocean_heat_transport_as_residual
+from .eval_ocean import fesom_ocean_heat_transport_as_residual, fesom_timeseries_of_mean_vertical_profile_in_region
