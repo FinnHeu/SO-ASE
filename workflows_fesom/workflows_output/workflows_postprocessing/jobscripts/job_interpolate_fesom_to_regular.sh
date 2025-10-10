94eece751630a2a89662eadcb029a6db15e48dee
FinnHeu <Finn.Heukamp@t-online.de>
2025-10-10 16:44:43 +0200

    Albedo to Levante
    Adjust the jobscript for use on Levante

--- workflows_fesom/workflows_output/workflows_postprocessing/jobscripts/job_interpolate_fesom_to_regular.sh
+++ workflows_fesom/workflows_output/workflows_postprocessing/jobscripts/job_interpolate_fesom_to_regular.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-#SBATCH --account=nwg_so-ase.nwg_so-ase
-#SBATCH --job-name=interpolate_parallel
-#SBATCH --partition=mpp
-#SBATCH --time=00:30:00
-#SBATCH --qos=30min
+#SBATCH --job-name=interpolate_fesom
+#SBATCH -p compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:30:00
+#SBATCH -o slurm-out.out
+#SBATCH -e slurm-err.out
+#SBATCH -A ab0995
 
 # Set library path so pyproj finds compatible libstdc++
-export LD_LIBRARY_PATH=$HOME/.conda/envs/pyfesom2/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$HOME/.conda/envs/pyfesom2/lib:$LD_LIBRARY_PATH
 
 # Output directory for logs
 LOG_DIR=logs_interpolate
 mkdir -p "$LOG_DIR"
 
 ############ Define Parameters ############
-base_path_model="/albedo/work/user/fheukamp/PostDoc1/FramStraitWSC/results_CTRL_cvmix/"
-base_path_mesh="$HOME/mesh/Arc01/" 
-year_start=2000
-year_end=2005
-variables_to_interpolate=("unod" "vnod" "temp" "salt")  
+base_path_model="/work/ab0995/a270186/esm_tools/runtime/awicm3-develop/CORE2ice_TEST/outdata/fesom/"
+base_path_mesh="/work/ab0995/a270186/model_inputs/fesom2/mesh/CORE2ice/" 
+year_start=1859
+year_end=1859
+variables_to_interpolate=("unod" "vnod" "temp" "salt" "fh" "fw" "sst")  
 min_lat=-90
-max_lat=90
+max_lat=-50
 min_lon=-180
 max_lon=180
-lat_increment=1.0
-lon_increment=1.0
+lat_increment=0.25
+lon_increment=0.25
 
 LOG_DIR="./logs/interpolate/"
 mkdir -p "$LOG_DIR"  # Ensure log directory exists
@@ -34,7 +34,7 @@ mkdir -p "$LOG_DIR"  # Ensure log directory exists
 # Loop over years
 for year in $(seq $year_start $year_end); do
     echo "Launching interpolation for year $year"
-    ~/.conda/envs/pyfesom2/bin/python ./../python/interpolate_fesom_to_regular.py \
+    ~/.conda/envs/pyfesom2_dev/bin/python ./../python/interpolate_fesom_to_regular.py \
         "$year" "$base_path_model" "$base_path_mesh" \
         "$min_lat" "$max_lat" "$min_lon" "$max_lon" \
         "$lat_increment" "$lon_increment" \
