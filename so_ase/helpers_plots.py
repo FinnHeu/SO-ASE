e2c21917405bd46be6ec41ee8c5cc6afc0292f49
FinnHeu <Finn.Heukamp@t-online.de>
2025-12-15 12:41:04 +0100

    Added and modified various functions

--- so_ase/helpers_plots.py
+++ so_ase/helpers_plots.py
@@ -19,3 +19,46 @@ def add_notebook_path_to_fig(fig, y_position=-0.05, fontsize=8):
     # Add notebook path as text at the lower end of the figure
     fig.text(0.5, y_position, f"Generated from: {notebook_path}", wrap=True, 
              horizontalalignment='center', fontsize=fontsize)
+    
+
+def remove_axes_frame(ax, left=True, right=True, top=True, bottom=True,
+                      ticks=True, labels=True):
+    """
+    Remove or hide selected sides of a matplotlib Axes frame.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The axes to modify.
+    left, right, top, bottom : bool, optional
+        If True, remove the corresponding spine.
+    ticks : bool, optional
+        If True, remove ticks on removed spines.
+    labels : bool, optional
+        If True, remove tick labels on removed spines.
+    """
+
+    sides = {
+        "left": left,
+        "right": right,
+        "top": top,
+        "bottom": bottom,
+    }
+
+    for side, remove in sides.items():
+        if remove:
+            ax.spines[side].set_visible(False)
+
+    if ticks:
+        if left:
+            ax.yaxis.set_ticks_position("none")
+        if bottom:
+            ax.xaxis.set_ticks_position("none")
+
+    if labels:
+        if left:
+            ax.set_yticklabels([])
+        if bottom:
+            ax.set_xticklabels([])
+
+    return
