0ee8678646ba80ee91c793ef7551c8c1c7bdbbd2
FinnHeu <Finn.Heukamp@t-online.de>
2026-02-09 22:19:25 +0100

    job execution with srun

--- workflows_fesom/workflows_output/workflows_postprocessing/jobscripts/job_interpolate_fesom_to_regular.sh
+++ workflows_fesom/workflows_output/workflows_postprocessing/jobscripts/job_interpolate_fesom_to_regular.sh
@@ -11,8 +11,8 @@
 #export LD_LIBRARY_PATH=$HOME/.conda/envs/pyfesom2/lib:$LD_LIBRARY_PATH
 
 ############ Define Parameters ############
-base_path_model="/work/ab0995/a270186/esm_tools/runtime/awicm3-develop/production_OSM26/PICTRL_CORE2_1650_1850_2/outdata/fesom/"
-base_path_mesh="/work/ab0995/a270186/model_inputs/fesom2/mesh/CORE2/" 
+base_path_model="/work/ab0995/a270186/esm_tools/runtime/awicm3-develop/production_OSM26/PICTRL_CORE2ice_1650_1850/outdata/fesom/"
+base_path_mesh="/work/ab0995/a270186/model_inputs/fesom2/mesh/CORE2ice/" 
 variables_to_interpolate=("a_ice" "sst" "sss" "MLD1" "MLD2" "MLD3" "thdgrice" "dyngrice" "fw_ice")  
 min_lat=-90
 max_lat=-40
@@ -25,18 +25,8 @@ radius_of_influence=100000 # meters
 LOG_DIR="./logs/interpolate/"
 mkdir -p "$LOG_DIR"  # Ensure log directory exists
 
-
-# Process all years in parallel using srun
-#  for year in {1650..1849}; do
-#      srun --ntasks=1 --nodes=1 ~/.conda/envs/so_ase/bin/python ./../python/interpolate_fesom_to_regular.py \
-#        "$year" "$base_path_model" "$base_path_mesh" \
-#        "$min_lat" "$max_lat" "$min_lon" "$max_lon" \
-#        "$lat_increment" "$lon_increment" "$radius_of_influence" \
-#        "${variables_to_interpolate[@]}" \
-#        > "${LOG_DIR}/log_${year}.log" 2>&1 &
-#  done
-
-MAX_PARALLEL=128
+# Process all years in parallel using srun  
+MAX_PARALLEL=128 # Make sure this is not higher than the number of tasks since the job will get stuck otherwise
 i=0
 
 for year in {1650..1849}; do
